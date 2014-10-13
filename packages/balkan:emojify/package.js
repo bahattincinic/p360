3607f245c1ee83a304f4fbd433076157711d4b3a
Burak Alkan @vmware-ubuntu <balkan@burakalkan.com>
2014-10-13 04:09:59 +0300

    install emojify

--- packages/balkan:emojify/package.js
+++ packages/balkan:emojify/package.js
@@ -8,6 +8,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
   api.use(['templating', 'underscore']);
-  api.addFiles(['balkan:emojify.js'], 'client');
+  api.addFiles(['emojify.js'], 'client');
 });
 
