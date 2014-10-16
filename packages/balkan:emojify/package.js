18bd7c0cad7e8d5a7edd40a7d3de7b45e979efa8
Bahattin Cinic <bahattincinic@gmail.com>
2014-10-16 18:59:56 +0300

    emojify package add

---
+++ packages/balkan:emojify/package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'balkan:emojify',
+  summary: ' emojifyjs ',
+  version: '1.0.0',
+  git: 'git://helevelevelvele'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('METEOR@0.9.3.1');
+    api.use(['templating', 'underscore']);
+    api.addFiles(['emojify.min.js', 'emojify.min.css', 'small.css'], 'client');
+
+    var fs = Npm.require("fs"),
+        path = Npm.require("path"),
+        imagesDir = 'images/emoji';
+    fs.readdir(imagesDir, function (err, files) {
+        if (err)
+          throw err;
+        files.forEach(function(file) {
+            api.add_files('images/emoji/' + file, 'client', {isAsset: true});
+        });
+    });
+});
+
