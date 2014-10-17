0003ad5834465acd6e5c5423700c6dfc25ff6d15
Burak Alkan <burak.alkan@markafoni.com>
2014-10-17 16:12:18 +0300

    update for blob

---
+++ packages/balkan:blob/balkan:blob.js
@@ -0,0 +1,105 @@
+(function(window) {
+
+  // set getUserMedia first and foremost
+  if (!navigator.getUserMedia) {
+    navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  }
+
+  var Recorder = function(source, cfg, recording) {
+    // read embedded worker code from blob.html
+    var blob = new Blob([
+      document.querySelector("script[type=\"text\/worker\"]").textContent
+      ], {type: "text/javascript"});
+    var config = cfg || {};
+    var bufferLen = config.bufferLen || 4096;
+    this.context = source.context;
+    this.node = (this.context.createScriptProcessor ||
+                 this.context.createJavaScriptNode).call(this.context,
+                                                         bufferLen, 2, 2);
+    // load blob into worker
+    var worker = new Worker(window.URL.createObjectURL(blob));
+    worker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate
+      }
+    });
+
+    var currCallback;
+
+    this.node.onaudioprocess = function(e){
+      if (!recording.getRecording()) return;
+      worker.postMessage({
+        command: 'record',
+        buffer: [
+          e.inputBuffer.getChannelData(0),
+          e.inputBuffer.getChannelData(1)
+        ]
+      });
+    }
+
+    this.configure = function(cfg) {
+      for (var prop in cfg) {
+        if (cfg.hasOwnProperty(prop)) {
+          config[prop] = cfg[prop];
+        }
+      }
+    }
+
+    this.record = function() {
+      recording.setRecording(true);
+    }
+
+    this.stop = function() {
+      recording.setRecording(false);
+    }
+
+    this.clear = function() {
+      worker.postMessage({ command: 'clear' });
+    }
+
+    this.getBuffer = function(cb) {
+      currCallback = cb || config.callback;
+      worker.postMessage({ command: 'getBuffer' })
+    }
+
+    this.exportWAV = function(cb, type) {
+      currCallback = cb || config.callback;
+      type = type || config.type || 'audio/wav';
+      if (!currCallback) throw new Error('Callback not set');
+      worker.postMessage({
+        command: 'exportWAV',
+        type: type
+      });
+    }
+
+    worker.onmessage = function(e) {
+      var blob = e.data;
+      currCallback(blob);
+    }
+
+    source.connect(this.node);
+    this.node.connect(this.context.destination);    //this should not be necessary
+  };
+
+  Recorder.forceDownload = function(blob, filename){
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+    var link = window.document.createElement('a');
+    link.href = url;
+    link.download = filename || 'output.wav';
+    var click = document.createEvent("Event");
+    click.initEvent("click", true, true);
+    link.dispatchEvent(click);
+  }
+
+  window.Recorder = Recorder;
+  // Addendum for recorderjs
+  // taken from RecordRTC
+  navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  // WebAudio API representer
+  if (!window.AudioContext) {
+      window.AudioContext = window.webkitAudioContext || window.mozAudioContext;
+  }
+
+})(window);
